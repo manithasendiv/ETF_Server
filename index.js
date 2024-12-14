7a3f86ffe94cd4a9bdc743cb768df4c0731030d6
BuddhimaDilini <buddhimadilini0819@gmail.com>
2024-12-14 18:44:44 +0530

    new endpoint

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get('/students', async (req, res) =>{
+ res.send("student end point")
+});
+
 app.listen(3000, function () {
   console.log("App listening on port 3000!");
 });
+
