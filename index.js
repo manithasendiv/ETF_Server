91d82eea43cddd3c2b980675c8cd64967a1e9bbf
manithasendiv <manitha20030809@gmail.com>
2024-12-14 18:12:34 +0530

    first_commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(3000, function () {
+  console.log("App listening on port 3000!");
+});
