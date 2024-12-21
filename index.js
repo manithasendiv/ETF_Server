0f0d9e6ae86f511fd752d74660cc79e407cd492a
manithasendiv <manitha20030809@gmail.com>
2024-12-21 18:25:11 +0530

    First Commit

--- index.js
+++ index.js
@@ -16,6 +16,32 @@ app.get("/", (req, res) => {
   res.send("Welcome to SCU Students API");
 });
 
+app.get("/students/searchSID/:searchValue", async (req, res) => {
+  try {
+    const students = await Student.find({ sid: req.params.searchValue });
+    if (students.length > 0) {
+      res.status(200).json(students);
+    } else {
+      res.status(404).json({ message: "No student found with that SID" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error finding student" });
+  }
+});
+
+app.get("/students/searchEmail/:searchValue", async (req, res) => {
+  try {
+    const students = await Student.find({ email: req.params.searchValue });
+    if (students.length > 0) {
+      res.status(200).json(students);
+    } else {
+      res.status(404).json({ message: "No student found with that Email" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error finding student" });
+  }
+});
+
 app.listen(3000, function () {
   console.log("App listening on port 3000!");
 });
