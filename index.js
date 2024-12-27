0ed6f01d6defcce5ae5d6cb058e617d579615da4
RNAGIT <ransanilakshayar2004@gmail.com>
2024-12-27 12:41:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,54 @@ app.get("/", (req, res) => {
   res.send("Welcome to SCU Students API");
 });
 
+
+
+// Add new result to the database
+app.post("/results", (req, res) => {
+  const result = new Results(req.body);
+
+  result.save()
+    .then(() => {
+      res.json({
+        message: "New result added successfully."
+      });
+    })
+    .catch((error) => {
+      res.json({
+        message: "Failed to add new result.",
+        error: error.message
+      });
+    });
+});
+
+
+
+// Retrieve a single result using the sid
+app.get("/results/:sid", (req, res) => {
+  const sid = req.params.sid; // Extract the sid from the request parameters
+
+  // Find the result using the sid
+  Results.findOne({ sid: sid })
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          message: "Result not found for the provided SID."
+        });
+      }
+      res.status(200).json({
+        message: "Result retrieved successfully.",
+        result: result 
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Failed to retrieve result.",
+        error: error.message
+      });
+    });
+});
+
+
 app.listen(3000, function () {
   console.log("App listening on port 3000!");
 });
